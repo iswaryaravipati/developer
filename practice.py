5efe3a446c9fb571dbe9cafcee400d6ca0a9bf6c
Iswaryaravipati <iswaryaravipati2004@gmail.com>
2026-02-26 14:55:53 +0530

    developer

---
+++ practice.py
@@ -0,0 +1,10 @@
+count=1
+n=int(input("enter a number:"))
+
+for i in range(n):
+    #alph=65
+    for j in range(i,-1,-1):
+        print(n-j,end="")
+        #count+=1
+        #alph+=1
+    print()
